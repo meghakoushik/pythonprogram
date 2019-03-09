d0d61cce1b8dc0e9c657d2cdd5c8c87d5f66c664
saurabh323351 <singh.saurab3333@gamil.com>
2019-03-09 10:02:26 +0530

    print first and last name reverse

---
+++ Prog1.py
@@ -0,0 +1,17 @@
+# Python program to Reverse First and last name
+
+A = input ( "Enter First name")
+B = input ("Enter Last name")
+
+
+#Display the Reverse name
+
+print(B+ "  " + A)
+
+
+
+
+
+#second method : if you want reverse name seprately
+
+print(A [::-1]+ "" + B [::-1])
