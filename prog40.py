6b31119b78a8f21bec7bd69fb6149364cff002ea
saurabh323351 <singh.saurab3333@gamil.com>
2019-03-09 11:29:58 +0530

    To extract single key value pair in dictionary

---
+++ prog40.py
@@ -0,0 +1,13 @@
+#program to extract single key value pair in dictionary
+
+
+dic_1 = {1: 0.34, 2: 0.54, 9: 67.8, 3: 6.77, 6: 9.99}
+
+#to extract key into the dic_1 list
+for key in dic_1.keys():
+ print(key)
+
+  
+#to extract value into the dic_1 list
+for value in dic_1.values():
+ print(value)
