7b425c610ad7e6e153f9175322951da5b261dead
saurabh323351 <singh.saurab3333@gamil.com>
2019-03-09 10:15:18 +0530

    print first and last color name from list

---
+++ prog3.py
@@ -0,0 +1,15 @@
+
+# tHIS PROGRAM TO PRINT THE FIRST AND LAST COLORS NAME FROM GIVEN LIST
+
+
+
+color_list = ['Red','Green','White','Black']
+
+
+# display the first color Name
+print(color_list [0])
+
+# display the first color Name
+print(color_list [-1])
+
+
