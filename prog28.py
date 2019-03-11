04d22060dd6c5afc102c69bc85c0a7343ba6a0b3
meghakoushik <koushikmegha011@gmail.com>
2019-03-11 10:05:57 +0530

    program to clear screen or terminal

---
+++ prog28.py
@@ -0,0 +1,12 @@
+#program to clear the screen or terminal.
+
+from os import system, name
+from time import sleep
+# define our clear function
+def clear():
+   if namle == 'nt':
+      _ = system('clear')
+
+sleep(5)
+# now call function we defined above
+clear()
