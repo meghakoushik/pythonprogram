523f63752b8e7bcb8054c339fbc9d7d4277cfbd1
meghakoushik <koushikmegha011@gmail.com>
2019-03-16 18:02:37 +0530

    FIRST AND LAST COLORS NAME FROM GIVEN LIST

---
+++ week1/prog3.py
@@ -0,0 +1,9 @@
+# THIS PROGRAM TO PRINT THE FIRST AND LAST COLORS NAME FROM GIVEN LIST
+color_list = ['Red', 'Green', 'White', 'Black']
+
+# display the first color Name
+print(color_list[0])
+# display the first color Name
+print(color_list[-1])
+
+
