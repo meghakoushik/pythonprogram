83f2e6b24c77ee34e9a2bed6e23e7346c9aff4e3
meghakoushik <koushikmegha011@gmail.com>
2019-03-16 18:07:12 +0530

    to print abs value with in-built function

---
+++ week1/prog4.py
@@ -0,0 +1,19 @@
+# Program to print abs value with in-built function
+
+# output is 5
+print(abs(-5))
+
+# without in-built function
+
+num = -8
+
+if num < 0:
+
+    num = num * (-1)
+
+    print(num)
+
+
+else:
+    num = num * (1)
+    print(num)
